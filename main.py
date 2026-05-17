17d37620868bc73fda4d4e850fc2878f7645d28f
Md. Maruful Islam <marufulislam0098@gmail.com>
2026-05-17 11:03:22 +0600

    Refactor target input and update scan sequence

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from modules.report import generate_report
-from modules.nuclei_scan import run_nuclei
-from modules.nikto_scan import run_nikto
-from modules.jsfinder import find_js
-from modules.crawler import crawl_site
-from modules.subdomain import find_subdomains
 from modules.portscan import scan_ports
-target = input("Enter Target: ")
+from modules.subdomain import find_subdomains
+from modules.crawler import crawl_site
+from modules.jsfinder import find_js
+from modules.techdetect import detect_technology, get_headers
+from modules.nikto_scan import run_nikto
+from modules.nuclei_scan import run_nuclei
+from modules.report import generate_report
 
-print("Target Selected:", target)
-print("Starting Scan...")
-
-target = input("Enter Target: ")
+target = input("Enter Target URL: ")
 
 find_subdomains(target)
 scan_ports(target)
+
 crawl_site(target)
 find_js(target)
+
+detect_technology(target)
+get_headers(target)
+
 run_nikto(target)
 run_nuclei(target)
+
 generate_report(target)
